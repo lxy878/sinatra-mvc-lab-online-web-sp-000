daaaee8e22723be962141b30e5c55623f1e97633
Xiaoyi Liu <lxy878@gmail.com>
2020-04-23 09:55:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,19 +8,18 @@ class PigLatinizer
 
   def piglatinize
     text_array = text.split(' ')
-    
+
     fixed_text =text_array.collect do |text|
-      a = text_array[2]
       # find first vowel position
-      vowel_index = a.index(/[aeiou]/)
-      last = a.length-1
+      vowel_index = text.index(/[aeiou]/)
+      last = text.length-1
       new_word = ""
       # if the first vowel is not 0
       if !vowel_index.zero?
         # cut the position from 0 to position-1
-        prefix = a[0..vowel_index-1]
+        prefix = text[0..vowel_index-1]
         # add cut piece to the end of text
-        new_word = a[vowel_index..last]+prefix
+        new_word = text[vowel_index..last]+prefix
       # otherwise
       else
         # add 'w'
